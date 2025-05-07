621b17f6ec6379361911131bd2123700bcc01606
Nina Lubrino <clubrino@gmail.com>
2025-05-06 22:09:55 -0700

    Renamed films to movies

---
+++ routes/movies.js
@@ -0,0 +1,15 @@
+/**
+ * movies.js
+ * 04-29-2025- Modified by Cielina Lubrino
+ */
+
+const express = require('express');
+const router = express.Router();
+const Title = require('../models/Title');
+
+router.get('/', async (req, res) => {
+    const titles = await Title.find().sort({ name: 'asc' }).limit(10);
+    res.render('movies', { title: 'Movies', titles });
+});
+
+module.exports = router;
